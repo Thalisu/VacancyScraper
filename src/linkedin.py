924d17db95cbcbdd91767480b2fd4c876f7897e3
Thalisu <thalisondmcc@gmail.com>
2024-10-02 16:04:34 -0300

    feat: add linkedin app

---
+++ src/linkedin.py
@@ -0,0 +1,12 @@
+import sys
+from scrappers.linkedin_scrapper import LinkedinScrapper
+
+keywords = sys.argv[1] or "desevolvedor"
+location = sys.argv[2] or "Brazil"
+timeframe = sys.argv[3] or "r86400"
+remote = sys.argv[4] or "1%2C2%2C3"
+page = sys.argv[5] or 1
+
+SCRAPPER = LinkedinScrapper(keywords, location, timeframe, remote)
+
+print(SCRAPPER.get_jobs(page))
