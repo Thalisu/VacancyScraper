fbe55fe57377a26ae6a428da1324dbde4d5d9ad6
Thalisu <thalisondmcc@gmail.com>
2024-10-14 12:42:17 -0300

    refactor: change class to func

--- src/linkedin.py
+++ src/linkedin.py
@@ -1,5 +1,5 @@
 import sys
-from scrappers.linkedin_scrapper import LinkedinScrapper
+from scrappers.linkedin_scrapper import get_linkedin_jobs
 
 keywords = sys.argv[1] or "desenvolvedor"
 location = sys.argv[2] or "Brazil"
@@ -7,6 +7,4 @@ timeframe = sys.argv[3] or "r86400"
 remote = sys.argv[4] or "1%2C2%2C3"
 page = sys.argv[5] or 0
 
-SCRAPPER = LinkedinScrapper(keywords, location, timeframe, remote)
-
-print(SCRAPPER.get_jobs(page))
+print(get_linkedin_jobs(keywords, location, timeframe, remote, page))
