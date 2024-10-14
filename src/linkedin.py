43c4ad546f61e305a7fa198e99a6898eb2431dd7
Thalisu <thalisondmcc@gmail.com>
2024-10-14 12:18:31 -0300

    feat: add page handling

--- src/linkedin.py
+++ src/linkedin.py
@@ -1,11 +1,11 @@
 import sys
 from scrappers.linkedin_scrapper import LinkedinScrapper
 
-keywords = sys.argv[1] or "desevolvedor"
+keywords = sys.argv[1] or "desenvolvedor"
 location = sys.argv[2] or "Brazil"
 timeframe = sys.argv[3] or "r86400"
 remote = sys.argv[4] or "1%2C2%2C3"
-page = sys.argv[5] or 1
+page = sys.argv[5] or 0
 
 SCRAPPER = LinkedinScrapper(keywords, location, timeframe, remote)
 
--- src/scrappers/linkedin_scrapper.py
+++ src/scrappers/linkedin_scrapper.py
@@ -5,7 +5,7 @@ import json
 
 
 class LinkedinScrapper:
-    BASE_JOBS_URL = "https://www.linkedin.com/jobs/search?"
+    BASE_JOBS_URL = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?"
 
     def __init__(
         self,
@@ -24,25 +24,33 @@ class LinkedinScrapper:
 
     def get_jobs(self, page):
         i = 0
-        re = requests.get(self.url)
+        URL = f"{self.url}&start={page * 25}"
+        re = requests.get(URL)
         while re.status_code != 200 and i < 5:
-            re = requests.get(self.url)
+            re = requests.get(URL)
             i += 1
             time.sleep(0.2)
-
         if re.status_code != 200:
             return json.dumps([{"error": "timeout"}], indent=4)
 
         soup = BeautifulSoup(re.text, "html.parser")
-        job_cards = soup.findAll("div", attrs={"class": "job-search-card"})
-        jobs_dict = [
-            {
-                "title": job.find("h3").text.strip(),
-                "enterprise": job.find("h4").text.strip(),
-                "url": job.find("a")["href"],
-                "img": job.find("img")["data-delayed-url"],
-            }
-            for job in job_cards
-        ]
-
-        return json.dumps(jobs_dict, indent=4)
+        job_cards = soup.findAll(
+            ["div", "a"], attrs={"class": "job-search-card"}
+        )
+        try:
+            jobs_dict = [
+                {
+                    "title": job.find("h3").text.strip(),
+                    "enterprise": job.find("h4").text.strip(),
+                    "url": (
+                        job.find("a")["href"]
+                        if job.find("a")
+                        else job.get("href")
+                    ),
+                    "img": job.find("img")["data-delayed-url"],
+                }
+                for job in job_cards
+            ]
+            return json.dumps(jobs_dict, indent=4)
+        except TypeError as err:
+            return json.dumps([{"TypeError": err.args[0]}], indent=4)
