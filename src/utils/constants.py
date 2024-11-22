d91583597512091b300123974b60dba8e0c0489d
Thalisu <thalisondmcc@gmail.com>
2024-11-21 23:51:34 -0300

    feat: add constants

---
+++ src/utils/constants.py
@@ -0,0 +1,5 @@
+LINKEDIN_JOBS_URL = "https://www.linkedin.com/jobs/search/?"
+
+LINKEDIN_SIGNIN_URL = "https://www.linkedin.com/uas/login?trk=guest_homepage-basic_nav-header-signin"
+
+LINKEDIN_AUTH_URL = "https://www.linkedin.com/checkpoint/lg/login-submit"
