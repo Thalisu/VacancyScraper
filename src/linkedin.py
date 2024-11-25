2565911a9759de9bec72cd1f76682e38c7988aad
Thalisu <thalisondmcc@gmail.com>
2024-11-25 12:26:46 -0300

    feat: temporary access to scraper via sys argvs

--- src/linkedin.py
+++ src/linkedin.py
@@ -1,10 +1,17 @@
 import sys
-from scrappers.linkedin_scrapper import get_linkedin_jobs
+from scrapers.linkedin_scraper import LinkedinJobs
 
-keywords = sys.argv[1] or "desenvolvedor"
+keywords = sys.argv[1]
+if not keywords:
+    print("Keywords are required")
+    sys.exit(0)
 location = sys.argv[2] or "Brazil"
 timeframe = sys.argv[3] or "r86400"
 remote = sys.argv[4] or "1%2C2%2C3"
 page = sys.argv[5] or 0
 
-print(get_linkedin_jobs(keywords, location, timeframe, remote, page))
+jobs = LinkedinJobs(keywords, location, timeframe, remote)
+
+job_list = jobs.get(page)
+
+print(job_list)
