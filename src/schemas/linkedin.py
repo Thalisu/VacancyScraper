e5634726512c3a988d96e594d0b79464ffff30b9
Thalisu <thalisondmcc@gmail.com>
2024-12-03 12:24:16 -0300

    feat: add jobrequest schema

---
+++ src/schemas/linkedin.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+
+
+class JobRequest(BaseModel):
+    keywords: str = Field(min_length=3, max_length=255)
+    location: str = Field(default="Brazil", max_length=32)
+    timeframe: str = Field(default="r86400", max_length=16)
+    remote: str = Field(default="1%2C2%2C3", max_length=9)
+    page: int = Field(default=0)
