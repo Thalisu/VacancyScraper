a0c644a4781502f788b9efa329a60e13e073c14b
Thalisu <thalisondmcc@gmail.com>
2024-12-29 20:46:53 -0300

    feat: add an optional error field for errors in responses

--- src/schemas/linkedin.py
+++ src/schemas/linkedin.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel, Field
 
+from src.models.job import Job
+from src.models.error import Error
+
+from typing import List, Optional
+
 
 class JobRequest(BaseModel):
     keywords: str = Field(min_length=3, max_length=255)
@@ -7,3 +12,9 @@ class JobRequest(BaseModel):
     timeframe: str = Field(default="r86400", max_length=16)
     remote: str = Field(default="1%2C2%2C3", max_length=9)
     page: int = Field(default=0)
+
+
+class JobResponse(BaseModel):
+    keywords: str
+    error: Optional[Error]
+    jobs: List[Optional[Job]]
