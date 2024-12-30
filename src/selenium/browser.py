b7e724fee16762e3343027465318064f3eda3269
Thalisu <thalisondmcc@gmail.com>
2024-12-30 20:21:29 -0300

    perf: remove all time.sleep

--- src/selenium/browser.py
+++ src/selenium/browser.py
@@ -3,53 +3,46 @@ from selenium.common.exceptions import InvalidCookieDomainException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from src.exceptions import InvalidCookiesOrUrl, CantEnsureRequest
 
-import sys
-import time
+import asyncio
 
 
 class Browser:
     def __init__(self):
         self.driver = None
 
-    def create_driver(self, headless=False):
+    def create_driver(self, headless) -> Driver:
         if self.driver:
-            self.close()
+            return self.driver
 
         driver = Driver(uc=True, headless=headless)
         self.driver = driver
 
         return driver
 
-    def get_with_cookies(
-        self, base_url, url, cookies: list, ensured=True, retries=2
-    ):
-        self.driver.get(base_url)
-
+    async def add_cookies_to_domain(self, base_url, cookies: list, retries=2):
         if retries == 0:
-            print("cant put cookies")
-            sys.exit(0)
+            raise InvalidCookiesOrUrl(message="cookies cant be applied")
+
+        self.driver.get(base_url)
 
         try:
             for cookie in cookies:
                 self.driver.add_cookie(cookie)
         except InvalidCookieDomainException:
-            time.sleep(1)
-            self.get_with_cookies(base_url, url, cookies, ensured, retries - 1)
+            await asyncio.sleep(1)
+            await self.add_cookies_to_domain(base_url, cookies, retries - 1)
         finally:
-            if ensured:
-                self.ensured_get(url=url)
-            else:
-                self.driver.get(url)
+            return
 
-    def ensured_get(self, url):
+    async def ensured_get(self, url):
         self.driver.get(url)
 
-        if not self.__is_success(url=url):
-            print("cant get")
-            sys.exit(0)
+        if not await self.__is_success(url=url):
+            raise CantEnsureRequest(f"Failed to ensure connection to {url}")
 
-    def __is_success(self, url, retries=1, max_retries=5):
+    async def __is_success(self, url, retries=1, max_retries=5):
         element = WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
@@ -60,7 +53,8 @@ class Browser:
         if not retries <= max_retries:
             return False
 
-        time.sleep(retries * 0.5)
+        await asyncio.sleep(1)
+
         self.driver.get(url)
         return self.__is_success(url=url, retries=retries + 1)
 
