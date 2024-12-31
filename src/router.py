658af8e7bc172d613b5fa2b86bcc948d2339565b
Thalisu <thalisondmcc@gmail.com>
2024-12-30 23:31:45 -0300

    feat: add healthcheck route

--- src/router.py
+++ src/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from src.routes.healthcheck import router as healthcheck_router
 from src.routes.linkedin import router as linkedin_router
 
 router = APIRouter()
 
+router.include_router(healthcheck_router)
 router.include_router(linkedin_router)
