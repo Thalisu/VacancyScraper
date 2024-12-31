62c98d72d4d022a74baf1cfaa7a84dc03a06c774
Thalisu <thalisondmcc@gmail.com>
2024-12-31 12:23:42 -0300

    chore: add type hints

--- src/selenium/browser.py
+++ src/selenium/browser.py
@@ -1,4 +1,6 @@
 from seleniumbase import Driver  # type: ignore
+from selenium.webdriver.chrome.webdriver import WebDriver
+
 from selenium.common.exceptions import InvalidCookieDomainException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,40 +11,59 @@ import asyncio
 
 
 class Browser:
-    def __init__(self):
-        self.driver = None
+    def __init__(self) -> None:
+        self.driver: WebDriver | None = None
 
-    def create_driver(self, headless) -> Driver:
+    def create_driver(self, headless: bool) -> WebDriver:
         if self.driver:
             return self.driver
 
-        driver = Driver(uc=True, headless=headless)
-        self.driver = driver
+        driver: WebDriver = Driver(uc=True, headless=headless)  # type: ignore
+        if not isinstance(driver, WebDriver):
+            raise Exception("Failed to create driver")
 
+        self.driver = driver
         return driver
 
-    async def add_cookies_to_domain(self, base_url, cookies: list, retries=2):
+    async def add_cookies_to_domain(
+        self, base_url: str, cookies: list[dict[str, str]], retries: int = 2
+    ) -> None:
         if retries == 0:
             raise InvalidCookiesOrUrl(message="cookies cant be applied")
+
+        if not self.driver:
+            self.driver = self.create_driver(headless=True)
 
         self.driver.get(base_url)
 
         try:
             for cookie in cookies:
-                self.driver.add_cookie(cookie)
+                self.driver.add_cookie(cookie)  # type: ignore
         except InvalidCookieDomainException:
             await asyncio.sleep(1)
             await self.add_cookies_to_domain(base_url, cookies, retries - 1)
         finally:
             return
 
-    async def ensured_get(self, url):
+    async def ensured_get(self, url: str) -> None:
+        if not self.driver:
+            self.driver = self.create_driver(headless=True)
+
         self.driver.get(url)
 
         if not await self.__is_success(url=url):
             raise CantEnsureRequest(f"Failed to ensure connection to {url}")
 
-    async def __is_success(self, url, retries=1, max_retries=5):
+    async def __is_success(
+        self, url: str, retries: int = 1, max_retries: int = 5
+    ) -> bool:
+
+        if not retries <= max_retries:
+            return False
+
+        if not self.driver:
+            self.driver = self.create_driver(headless=True)
+
         element = WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
@@ -50,14 +71,14 @@ class Browser:
         if "neterror" not in element.text:
             return True
 
-        if not retries <= max_retries:
-            return False
-
         await asyncio.sleep(1)
 
         self.driver.get(url)
-        return self.__is_success(url=url, retries=retries + 1)
+        return await self.__is_success(url=url, retries=retries + 1)
 
-    def close(self):
+    def close(self) -> None:
+        if not self.driver:
+            return
+
         self.driver.quit()
         self.driver = None
