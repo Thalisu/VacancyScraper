8274716c388ca5c17d18c5449e476ad962df9e82
Thalisu <thalisondmcc@gmail.com>
2025-01-01 20:57:25 -0300

    feat: add routes to enqueue a keyword search and get queued tasks

--- src/routes/linkedin.py
+++ src/routes/linkedin.py
@@ -1,21 +1,34 @@
 from fastapi import APIRouter, HTTPException
+from src.models.task import Task
+from src.schemas.task_id import TaskId
 from src.scrapers.linkedin_scraper import LinkedinJobs
-
+from src.redis.queue import enqueue, get_job, get_queue_size
 from src.schemas.linkedin import JobRequests
-from src.models.responses import LinkedInJobResponses
 
-from src.exceptions import MissingCookies, InvalidCookiesOrUrl
-
-router = APIRouter()
+router = APIRouter(prefix="/linkedin")
 
 
-@router.get("/linkedin")
-async def search_job(request: JobRequests) -> LinkedInJobResponses:
-    try:
-        jobs = LinkedinJobs(request)
-    except InvalidCookiesOrUrl:
-        raise HTTPException(status_code=400, detail="Invalid cookies or url")
-    except MissingCookies:
-        raise HTTPException(status_code=400, detail="missing authentication")
+async def get_linkedin_jobs(request: JobRequests):
+    return await LinkedinJobs(jobRequests=request).get()
 
-    return await jobs.get()
+
+@router.get("/")
+async def search_job(request: JobRequests) -> dict[str, str]:
+    task_id = enqueue(get_linkedin_jobs, request)
+
+    return task_id
+
+
+@router.get("/queue")
+async def test_enqueue() -> dict[str, int]:
+    return {"queue_size": get_queue_size()}
+
+
+@router.get("/get")
+async def get_task(request: TaskId) -> Task:
+    task = get_job(request.task_id)
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return task
