7a73cbee6ded13f2b1b871453edd9b60b350a54a
Thalisu <thalisondmcc@gmail.com>
2025-01-01 21:55:35 -0300

    chore: add env port

--- src/redis/worker.py
+++ src/redis/worker.py
@@ -1,7 +1,10 @@
 from rq import Worker, Queue
 from redis import Redis
 
-redis_conn = Redis(host="localhost", port=6379)
+from src.utils.config import get_config
+
+port: int = get_config("REDIS_PORT") or 6379
+redis_conn = Redis(host="localhost", port=port)
 
 worker = Worker(
     queues=[Queue(connection=redis_conn)],
